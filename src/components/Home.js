00a28a1241c5b362d3ebd1dae322e0672119377c
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-16 23:16:24 +0200

    Add Homepage basic layout.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
   return (
     <section className="homepage">
-      <Link to="/forex" className="category">Forex</Link>
-      <Link to="/majors" className="category">Majors</Link>
-      <Link to="/crypto" className="category">Cryptocurrencies</Link>
-      <Link to="/stocks" className="category">Stocks</Link>
-      <Link to="/commodities" className="category">Commodities</Link>
+      <Link to="/forex" className="category">
+        <div className="category-info">
+          <h2 className="category-title">Forex</h2>
+          <p className="total">136</p>
+        </div>
+      </Link>
+      <Link to="/majors" className="category">
+        <div className="category-info">
+          <h2 className="category-title">Majors</h2>
+          <p className="total">5</p>
+        </div>
+      </Link>
+      <Link to="/crypto" className="category">
+        <div className="category-info">
+          <h2 className="category-title">
+            Crypto
+            {' '}
+            <br />
+            {' '}
+            Currencies
+          </h2>
+          <p className="total">10</p>
+        </div>
+      </Link>
+      <Link to="/stocks" className="category">
+        <div className="category-info">
+          <h2 className="category-title">Stocks</h2>
+          <p className="total">36</p>
+        </div>
+      </Link>
+      <Link to="/commodities" className="category">
+        <div className="category-info">
+          <h2 className="category-title">Commodities</h2>
+          <p className="total">16</p>
+        </div>
+      </Link>
     </section>
   );
 }
