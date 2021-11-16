2b3ab904174d9e551d092132473e0f67fc3d9d0b
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-16 20:42:16 +0200

    Add action creators for stocks, crypto, and commodities.

--- src/redux/actions/markets.js
+++ src/redux/actions/markets.js
@@ -1,8 +1,11 @@
 import * as api from '../../api/api';
-import { GET_FOREX } from './actionTypes';
+import * as market from '../../components/data';
+import {
+  GET_FOREX, GET_CRYPTOS, GET_STOCKS, GET_COMMODITIES,
+} from './actionTypes';
 
 // API action creators
-const getFxPrices = () => async (dispatch) => {
+export const getFxPrices = () => async (dispatch) => {
   try {
     const data = await api.fetchFxPrices();
 
@@ -12,4 +15,47 @@ const getFxPrices = () => async (dispatch) => {
   }
 };
 
-export default getFxPrices;
+export const getCommodities = () => async (dispatch) => {
+  try {
+    const data = await api.fetchCommodities();
+
+    dispatch({ type: GET_COMMODITIES, payload: data });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getCryptos = () => (dispatch) => {
+  try {
+    const data = [];
+
+    market.cryptos.forEach(async (crypto) => {
+      const { symbol } = crypto;
+      const cryptoData = await api.fetchCryptos(symbol);
+
+      data.push(cryptoData);
+    });
+
+    dispatch({ type: GET_CRYPTOS, payload: data });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getStocks = () => (dispatch) => {
+  try {
+    const data = [];
+
+    market.stocks.forEach(async (stock) => {
+      const { name, symbol } = stock;
+      const stockData = await api.fetchStocks(symbol);
+      stockData.name = name;
+
+      data.push(stockData);
+    });
+
+    dispatch({ type: GET_STOCKS, payload: data });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
