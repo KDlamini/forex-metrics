1445040a124d91b91230ae8571f072d8530221e6
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-17 00:30:44 +0200

    Get commodities on page render.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Crypto from './components/Crypto';
 import Stocks from './components/Stocks';
+import Commodities from './components/Commodities';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/crypto" element={<Crypto />} />
         <Route path="/stocks" element={<Stocks />} />
+        <Route path="/commodities" element={<Commodities />} />
       </Routes>
     </div>
   );
