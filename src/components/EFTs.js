2e57ea2b66ddc22cfbecf5d36fdf0931cda430b1
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-18 01:05:03 +0200

    Render ETF list on Etf component. - Update homepage layout.

--- src/components/EFTs.js
+++ src/components/EFTs.js
@@ -14,9 +14,26 @@ function EFTs() {
   }, []);
 
   return (
-    <div>
-      EFTs
-    </div>
+    <section className="efts">
+      <ul className="forex-pairs">
+        { efts.map((eft) => {
+          const { name, price, symbol } = eft;
+
+          return (
+            <li key={symbol} className="eft">
+              <div className="eft-info">
+                <h3 className="eft-symbol">{symbol}</h3>
+                <p className="eft-name">{name}</p>
+              </div>
+
+              <div className="eft-prices">
+                <p>{`Price: $${price}`}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
