34f4cd504c6eaca5aed98be2fb3d68b97f17ce5c
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-17 21:23:29 +0200

    Render forex majors on MAjors component.

--- src/components/Majors.js
+++ src/components/Majors.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Majors() {
+  const forex = useSelector((state) => state.forex);
+  const majors = forex.filter((majors) => majors.ticker === 'EUR/USD'
+    || majors.ticker === 'GBP/USD'
+    || majors.ticker === 'USD/JPY'
+    || majors.ticker === 'USD/CAD'
+    || majors.ticker === 'USD/CHF'
+    || majors.ticker === 'AUD/USD'
+    || majors.ticker === 'NZD/USD');
+
   return (
-    <div>
-      Majors
-    </div>
+    <section className="forex">
+      <ul className="forex-pairs">
+        { majors.map((pair) => {
+          const {
+            ticker, ask, bid, changes,
+          } = pair;
+
+          return (
+            <li key={ticker} className="pair">
+              <h3 className="pair-name">{ticker}</h3>
+              <div className="pair-prices">
+                <p>{`Ask: ${ask}`}</p>
+                <p>{`Bid: ${bid}`}</p>
+                <p>{`Changes: ${changes}`}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
