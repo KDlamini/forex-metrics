0325aac8aa73d692461703b9ccd51919f0070122
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-18 21:01:23 +0200

    Import EFTs styles file.

--- src/components/EFTs.js
+++ src/components/EFTs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { categories } from './data';
+import '../styles/EFTs.css';
 
 function EFTs() {
   const efts = useSelector((state) => state.efts);
